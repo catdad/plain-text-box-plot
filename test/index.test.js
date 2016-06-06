ce77630e95e7c161556a7750dae9a884bdf654ed
Kiril Vatev <vatev.1@gmail.com>
2016-06-06 10:02:34 -0400

    validating actual strings of the box plot

--- test/index.test.js
+++ test/index.test.js
@@ -3,6 +3,24 @@
 var expect = require('chai').expect;
 var ptBox = require('../index.js');
 
+function isValidBoxPlot(str) {
+    expect(str).to.be.a('string');
+    
+    var strArr = str.split('\n');
+    
+    expect(strArr).to.have.lengthOf(3);
+    
+    var str1 = strArr[0];
+    var str2 = strArr[1];
+    var str3 = strArr[2];
+    
+    expect(str1).to.match(/^\s{1,}\+\-{0,}\+\-{0,}\+\s{1,}$/);
+    expect(str2).to.match(/^\|\-{0,}\|\s{0,}\|\s{0,}\|\-{0,}\|\s{0,}$/);
+    
+    // the top and bottom of the box plot are the same string
+    expect(str3).to.equal(str1);
+}
+
 describe('[index]', function() {
     var DATA = {
         min: 1,
@@ -26,6 +44,8 @@ describe('[index]', function() {
             expect(line).to.have.property('length')
                 .and.to.be.at.most(DEFAULT_WIDTH);
         });
+        
+        isValidBoxPlot(str);
     });
     
     it('takes an optional width', function() {
@@ -37,6 +57,8 @@ describe('[index]', function() {
             expect(line).to.have.property('length')
                 .and.to.be.at.most(WIDTH);
         });
+        
+        isValidBoxPlot(str);
     });
     
     it('takes an optional options object with a width property', function() {
@@ -50,5 +72,7 @@ describe('[index]', function() {
             expect(line).to.have.property('length')
                 .and.to.be.at.most(WIDTH);
         });
+        
+        isValidBoxPlot(str);
     });
 });
